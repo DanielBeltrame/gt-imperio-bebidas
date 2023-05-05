14ee95f19dc1146de73bb6fedf271294f05afb68
daniel beltrame <danielbeltramexy@gmail.com>
2023-05-05 09:51:59 -0300

    Add typed and adjust information container

--- src/components/homePage/HomePage.js
+++ src/components/homePage/HomePage.js
@@ -1,9 +1,24 @@
 import './HomePage.css';
 import homePageImage from './img/homePageImage.png'
-import Typical from 'react-typical'
+import Typed from 'typed.js';
 import { motion } from 'framer-motion'
+import { useEffect, useRef } from 'react';
 
 function HomePage() {
+    const el = useRef(null);
+
+    useEffect(() => {
+        const typed = new Typed(el.current, {
+            strings: ['O MELHOR PREÇO!', 'A MAIOR VARIEDADE!'],
+            typeSpeed: 50,
+        });
+
+        return () => {
+            // Destroy Typed instance during cleanup to stop animation
+            typed.destroy();
+        };
+    }, []);
+
     return (
         <div id="home" className='homeContainer'>
             <div className='imgContainerHome'>
@@ -14,11 +29,7 @@ function HomePage() {
                     transition={{ duration: 1.0 }}>
                     <b>GT IMPÉRIO BEBIDAS, </b>
                     <div>
-                        <Typical
-                            steps={['O MELHOR PREÇO!', 3000, 'A MAIOR VARIEDADE!', 3000]}
-                            loop={Infinity}
-                            wrapper="b"
-                        />
+                        <b ref={el} />
                     </div>
                 </motion.div>
             </div>
